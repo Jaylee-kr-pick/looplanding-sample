f7c1eff22d2ad35636837131b8d8f0057c27c208
JayLee <zxc689@naver.com>
2025-09-22 15:46:44 +0900

    11

--- api/submit-form.js
+++ api/submit-form.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
     // 2. 구글 시트에 데이터 추가
     await sheets.spreadsheets.values.append({
       spreadsheetId: process.env.SPREADSHEET_ID,
-      range: 'looplanding_data!A1:D1', // 시트 이름과 범위
+      range: 'looplanding_data', // 시트 이름과 범위
       valueInputOption: 'USER_ENTERED',
       resource: {
         values: [[name, email, phone]],
